db6a6a4d095df7df8dc2b74ed298360aad72de5a
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2019-01-17 21:32:58 +0300

    blink for f4

---
+++ stm32f4/main.cpp
@@ -0,0 +1,64 @@
+//#include "project_config.h"
+
+//#error "Setup"
+
+
+#include "mcu_support_package/inc/stm32f4xx.h"
+//#include "mcu_support_package/gcc/startup_stm32f407xx.s"
+
+
+//#define STM32F407xx
+// Перед использованием не забудьте выбрать микроконтроллер - с помощью дефайнов вида STM32F407xx и STM32F40_41xxx - на весь проект
+// И выбрать правильную частоту кварца, установив на весь проект дефайн HSE_VALUE=8000000 или HSE_VALUE=12000000
+// И выбрать правильный startup-файл с расширением .s в группе mcu_support_package
+
+
+#define BLINKING  1
+#define BUTTON    2
+
+
+#define CURRENT_PART 2
+
+#if CURRENT_PART == BLINKING
+
+int main(void)
+{
+
+	RCC->AHB1ENR = 0x8;
+	GPIOD->MODER = 0x1000000;
+	GPIOD->OTYPER = 0x0;
+	__disable_irq();
+	SysTick_Config(SystemCoreClock/15);	//blink rate 50 Hz (0,02000004 s)
+	
+	while(1) {
+		if (SysTick->CTRL & (1<<16) ) { 
+			GPIOD->ODR ^= 0x1000;
+		}
+	}
+}
+
+
+#elif CURRENT_PART == BUTTON
+int main(void)
+{
+
+	RCC->AHB1ENR = 0x8;
+	GPIOD->MODER = 0x1000000;
+	GPIOD->OTYPER = 0x0;
+	__disable_irq();
+	SysTick_Config(SystemCoreClock/15);	//blink rate 50 Hz (0,02000004 s)
+	
+	while(1) {
+		if (SysTick->CTRL & (1<<16) ) { 
+			GPIOD->ODR ^= 0x1000;
+		}
+	}
+	return 0;
+}
+
+
+#else
+
+    #error "You should define CURRENT_PART to enable some part of the assignment"
+
+#endif
